6afa20b3eb12d2301ea06289b0d34dd80464e20a
Sera Lee <ssellu.lee@gmail.com>
2021-05-01 16:18:28 +0900

    문제1~10

--- 문제300/문제1~10.py
+++ 문제300/문제1~10.py
@@ -1,5 +1,32 @@
 
-
 # 문제 1
+print("Hello World")
 
 # 문제 2
+print("Mary's cosmetics")
+
+# 문제 3
+print('신씨가 소리질렀다, "도둑이야" ')
+
+# 문제 4 : 엔터위에 원회기호 \
+print("C:\windows")
+
+# 문제 5
+print("안녕하세요.\n만나서\t\t반갑습니다")
+    # 설명 : \n : 줄바꿈  \t : 탭[들여쓰기]
+
+# 문제 6  : ,[쉼표] 구분 짓기
+print("오늘은","일요일")
+
+# 문제 7 : ;[세미콜론]
+print("naver;kakao;samsung")
+
+# 문제 8 :
+print("naver/kakao/sk/samsung")
+
+# 문제 9 : end='' 끝마침을 공백으로 함
+print("first" , end='' );print("second")
+
+# 문제 10
+print("5/3")
+
